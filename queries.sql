fafb3e175ff514a0d40634d43c21cb5e6f8c1a59
Michael Eisenbraun <eisenbm@algonquincollege.edu>
2019-07-24 12:17:47 -0400

    Adds the starter files

---
+++ queries.sql
@@ -0,0 +1,26 @@
+-- 1. Retrieve all of the columns and all of the rows from the `movies` table. 
+
+
+
+-- 2. Retrieve the movie title (`movie_title`) and movie director (`director`) with the movie id (`movie_id`) of `3` from the `movies` table.
+
+
+
+-- 3. Retrieve all of the movies with the word "the" in the title (`movie_title`) from the `movies` table.
+
+
+
+-- 4. Retrieve all of the movies whose title starts with the word "the" and whose year is after 1990 sorted by year starting with the newest movies. 
+
+
+
+-- 5. Retrieve the first 5 movie titles (`movie_title`), directors (`director`) and years (`year`) with the headings: Movie Title, Movie Director, and Release Year sorted alphabetically by the movie title.
+
+
+
+-- 6. Retrieve all the movies from the years 1982, 1999, and 2009 from the `movies` table.
+
+
+
+-- 7. Using a subquery, retrieve all the movie titles (`movie_title`) from the genre "Sci-Fi".
+
